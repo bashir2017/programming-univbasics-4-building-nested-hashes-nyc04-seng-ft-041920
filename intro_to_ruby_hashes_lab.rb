d9f87f9dcb85d71425d91df21750aba8ea1be96e
Bashir <bashiralhanshali@yahoo.com>
2020-04-24 01:18:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
   my_base_hash[:railroads][:pieces] = 4
   my_base_hash[:railroads][:rent_in_dollars] = {}
   my_base_hash[:railroads][:names] = {}
-  my_base_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  my_base_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 25
   my_base_hash
 
 end
