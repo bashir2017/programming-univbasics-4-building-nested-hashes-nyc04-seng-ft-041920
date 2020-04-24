d732a21036f482658b96e633d38a12ec517d5034
Bashir <bashiralhanshali@yahoo.com>
2020-04-24 01:28:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,9 @@ def monopoly_with_fourth_tier
   my_base_hash[:railroads][:names][:b_and_o_railroad] = {}
   my_base_hash[:railroads][:names][:shortline_railroad] = {}
   
-  my_base_hash[:railroads][:names][:reading_railroad][:] = {}
+  my_base_hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  
+  my_base_hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
   my_base_hash
   
 end
