d1ac24e7a3cf21d079d61cfc8bb2b48ea845fc0d
Bashir <bashiralhanshali@yahoo.com>
2020-04-24 01:14:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # complexity.
   my_base_hash = {:railroads => {}}
   my_base_hash[:railroads][:pieces] = 4
-  my_base_hash[:railroads][:pieces] = 4
+  my_base_hash[:railroads][:] = 4
   
 end
 
